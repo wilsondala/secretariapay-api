5361c48cf7028696442045d8f01a5dc975258f0d
Wilson Dala <99853733+wilsondala@users.noreply.github.com>
2026-07-06 05:35:29 -0300

    feat: liberar webhook AppyPay

--- src/main/java/com/secretariapay/api/config/SecurityConfig.java
+++ src/main/java/com/secretariapay/api/config/SecurityConfig.java
@@ -141,6 +141,7 @@ public class SecurityConfig {
                                                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                                                 .requestMatchers(HttpMethod.GET, "/api/v1/secretariapay/whatsapp/webhook").permitAll()
                                                 .requestMatchers(HttpMethod.POST, "/api/v1/secretariapay/whatsapp/webhook").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/v1/payments/appypay/webhook").permitAll()
                                                 .requestMatchers("/api/v1/admin/**")
                                                 .hasAnyAuthority("ADMIN", "ROLE_ADMIN")
 
