2ed62e262c79430aaac8563234844cb21b4d28f3
Wilson Dala <99853733+wilsondala@users.noreply.github.com>
2026-07-22 08:20:11 -0300

    feat: criar estados da candidatura

---
+++ src/main/java/com/secretariapay/api/entity/enums/admission/AdmissionApplicationStatus.java
@@ -0,0 +1,14 @@
+package com.secretariapay.api.entity.enums.admission;
+
+public enum AdmissionApplicationStatus {
+    DRAFT,
+    SUBMITTED,
+    DOCUMENTATION_PENDING,
+    AWAITING_PAYMENT,
+    PAYMENT_UNDER_REVIEW,
+    PAID,
+    CONFIRMED,
+    REJECTED,
+    CANCELLED,
+    EXPIRED
+}
