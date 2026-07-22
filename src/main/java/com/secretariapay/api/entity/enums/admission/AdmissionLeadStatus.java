1ba4ca3f332c8b84a469182bc34b6daf04d0f735
Wilson Dala <99853733+wilsondala@users.noreply.github.com>
2026-07-22 08:20:02 -0300

    feat: criar estados de leads de admissão

---
+++ src/main/java/com/secretariapay/api/entity/enums/admission/AdmissionLeadStatus.java
@@ -0,0 +1,13 @@
+package com.secretariapay.api.entity.enums.admission;
+
+public enum AdmissionLeadStatus {
+    NEW,
+    CONTACTED,
+    APPLICATION_STARTED,
+    APPLICATION_SUBMITTED,
+    CONVERTED_TO_APPLICANT,
+    NO_RESPONSE,
+    WITHDREW,
+    NOT_ELIGIBLE,
+    OPTED_OUT
+}
